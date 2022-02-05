39e1ffe7e67eb523629e9816ffb6ce8c4df5690f
Skyler Link <skylerlink96@gmail.com>
2022-02-05 11:06:10 -0500

    Created separate functions for the creation of the answer list, and the guess handler Added a function to create a separate word list for possible guesses which aren't possible answers

--- Wordle.py
+++ Wordle.py
@@ -1,54 +1,41 @@
 from csv import reader
 from random import randint
 
-# Opens our list of possible answers
-# List starts as a CSV file and is converted to one list of word strings (in uppercase)
-file = open('C:/Users/0996516/Downloads/wordle-answers-alphabetical.csv', 'r')
-csv_reader = reader(file)
-lists_from_csv = []
-for row in csv_reader:
-    word_string = ""
-    for letter in row:
-        word_string += letter.upper()
-    lists_from_csv.append(word_string)
 
-# Picks a random Word from our newly created Word-List
-word = lists_from_csv[randint(0, 2314)]
+# Takes the file path of the answer csv file and outputs a list of possible answer strings (all in uppercase)
+def answers_list_maker(file_path):
+    file = open(file_path, 'r')
+    csv_reader = reader(file)
+    lists_from_csv = []
+    for row in csv_reader:
+        word_string = ""
+        for letter in row:
+            word_string += letter.upper()
+        lists_from_csv.append(word_string)
+    return lists_from_csv
 
-# Initializes Guess to an empty string and starts on Guess #1
-guess = ""
-guess_count = 1
-good_hint = ["_", "_", "_", "_", "_"]
-okay_hint = []
-bad_hint = []
 
-# A loop that continues to ask the user for a new 5-letter word (or a "Guess")
-# You win after guessing the Word, you lose after 6 wrong Guesses
-# Guesses are automatically converted to uppercase so that they can be compared to our Word-List, which is all uppercase
-while guess != word:
-    if guess_count > 6:
-        print("You used all six guesses. You lose. The word was " + word + ".")
-        break
-    guess = input(f"Guess #{guess_count}: ").upper()
-    # The following 2 loops verify that the user's Guess is both a 5-letter word AND in the Word-List
-    # If either or both of these qualifications are not met, the program asks the user for a new Guess
-    # Since these Guesses are not possible answers, they don't count against the Guess-Count
-    while len(guess) != 5:
-        print("Guess must be 5 characters.")
-        print()
-        guess = input(f"Still Guess #{guess_count}: ").upper()
-    while guess not in lists_from_csv:
-        print("Word not in word list.")
-        print()
-        guess = input(f"Still Guess #{guess_count}: ").upper()
-    # Now that we've verified the current Guess is valid, the Guess-Counter is incremented
-    guess_count += 1
+# Takes the file path of the allowed guesses csv file and outputs a list of answer strings (all in uppercase)
+# These are words that aren't possible answers, but are still allowed as guesses
+def guesses_list_maker(file_path):
+    file = open(file_path, 'r')
+    csv_reader = reader(file)
+    lists_from_csv = []
+    for row in csv_reader:
+        word_string = ""
+        for letter in row:
+            word_string += letter.upper()
+        lists_from_csv.append(word_string)
+    return lists_from_csv
 
-    # Compares a valid Guess to the actual Word and prints indicators next to each Guess letter
-    # + means the letter is in the Word and in the right place
-    # o means the letter is in the Word, but in a different place
-    # x means the letter is not in the Word at all
+
+# Compares a valid Guess to the actual Word and prints indicators next to each Guess letter
+# + means the letter is in the Word and in the right place
+# o means the letter is in the Word, but in a different place
+# x means the letter is not in the Word at all
+def guess_handler(guess, word, good_hint, bad_hint, okay_hint):
     index_counter = 0
+
     for i in guess:
         if i == word[index_counter]:
             print(f"{i}(+) ", end="  ")
@@ -69,6 +56,7 @@ while guess != word:
     print()
     print()
 
+    # Convert hints from lists into strings, to print them cleaner
     clean_good_hint = ""
     for i in good_hint:
         clean_good_hint += i
@@ -78,8 +66,8 @@ while guess != word:
     for i in okay_hint:
         clean_okay_hint += i
 
-    bad_hint.sort()
     clean_bad_hint = ""
+    bad_hint.sort()
     for i in bad_hint:
         clean_bad_hint += i
 
@@ -89,5 +77,48 @@ while guess != word:
         print(f"Does not contain: {clean_bad_hint}")
     print()
 
-if guess == word:
-    print("You win!")
+    return good_hint, bad_hint, okay_hint
+
+
+def main():
+    guess = ""
+    guess_count = 1
+    guesses_list = guesses_list_maker('C:/wordle-allowed-guesses.csv')
+    answers_list = answers_list_maker('C:/wordle-answers-alphabetical.csv')
+    word = answers_list[randint(0, 2314)]
+    good_hint = ["_", "_", "_", "_", "_"]
+    okay_hint = []
+    bad_hint = []
+
+    # A loop that continues to ask the user for a new 5-letter word (or a "Guess")
+    # You win after guessing the Word, you lose after 6 wrong Guesses
+    # Guesses are automatically converted to uppercase so that they can be compared to our uppercase Word-List
+    while guess != word:
+        if guess_count > 6:
+            print("You used all six guesses. You lose. The word was " + word + ".")
+            break
+        guess = input(f"Guess #{guess_count}: ").upper()
+
+        # 2 loops to verify the (Guess is both a 5-letter word) AND (in either the Guess-List or the Answer-List)
+        # If either or both of these qualifications are not met, the program asks the user for a new Guess
+        # Since these Guesses are not possible answers, they don't count against the Guess-Count
+        while len(guess) != 5:
+            print("Guess must be 5 characters.")
+            print()
+            guess = input(f"Still Guess #{guess_count}: ").upper()
+        while (guess not in answers_list) and (guess not in guesses_list):
+            print("Word not in word list.")
+            print()
+            guess = input(f"Still Guess #{guess_count}: ").upper()
+
+        # Now that we've verified the current Guess is valid, the Guess-Counter is incremented
+        guess_count += 1
+
+        guess_handler(guess, word, good_hint, bad_hint, okay_hint)
+
+    if guess == word:
+        print("You win!")
+
+
+if __name__ == "__main__":
+    main()
